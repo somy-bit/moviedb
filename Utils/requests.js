e942472978b0ce6073dd5c39b6204e32dbdead7f
Sara <sara@Saras-MacBook-Pro.local>
2025-08-13 19:11:31 -0700

    update

--- Utils/requests.js
+++ Utils/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.API_KEY;
+const API_KEY = "8eac012782c4661f6e03e3d37133b1c6";
 
 export default {
 
--- pages/Results.js
+++ pages/Results.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+
 import Thumbnail from './Thumbnail.js'
 
 function Results({results}){
